a436369206d3f9321d090017ba9c5474a54f74de
Tom Gardiner <tom@teppen.io>
2021-07-29 23:02:32 -0500

    add content

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # cognito-agent
 Authenticate with AWS Cognito and retreive credentials to use in your environment
+
+## Usecase
+
+Testing authenticated API Gateway endpoints can be hard.  cognito-agent can be paired with [awscurl](https://github.com/okigan/awscurl/) to test API Gateway endpoints that use IAM authorization.
+
+Example:
+
+```bash
+tom@MacBook ~ curl https://alfgjvvi136.execute-api.us-east-1.amazonaws.com/dev/v1/pets
+{"message":"Missing Authentication Token"}
+
+tom@MacBook ~ eval "$(cognito-agent)"
+tom@MacBook ~ awscurl https://alfgjvvi136.execute-api.us-east-1.amazonaws.com/dev/v1/pets
+{ id: 1, name: max, type: dog}
+```
+
+## Installation
+
+```bash
+npm i -g cognito-agent
+```
+
+## Configuration
+
+cognito-agent can only be configured using environment variables.  If any config is missing, you will be prompted at runtime to enter the missing values.
+
+### Environment Variables
+
+| Variable                      | Description                 |
+| ----------------------------- | --------------------------- |
+| COGNITO_AGENT_USERNAME        | Cognito User Pool Username  |
+| COGNITO_AGENT_PASSWORD        | Cognito User Pool Password  |
+| COGNITO_AGENT_USERPOOL_ID     | Cognito User Pool Id        |
+| COGNITO_AGENT_CLIENT_ID       | Cogntio User Pool Client Id |
+| COGNITO_AGENT_IDENTITYPOOL_ID | Cognito Identity Pool Id    |
+
+### Cli prompts at runtime
+
+cognito-agent will prompt for any variable not found at runtime.
+
+For exampe, you may wish to provide the username/password on the cli interactively:
+
+```bash
+tom@MacBook ~ cognito-agent
+Username: tom@example.org
+Password: ********
+
+AWS_DEFAULT_REGION=us-east-1; export AWS_DEFAULT_REGION;
+AWS_ACCESS_KEY_ID=ASIAZV...; export AWS_ACCESS_KEY_ID;
+AWS_SECRET_ACCESS_KEY=hIrXEJI13k...; export AWS_SECRET_ACCESS_KEY;
+AWS_SESSION_TOKEN=IQoJb3JpZ...; export AWS_SESSION_TOKEN;
+```
+
+## How it works
+
+cognito-agent will log-in to the Cognito User Pool and then request a set of temporary credentials from the Cognito Identity Pool.  It spits out these credentials ready to be exported, much like ssh-agent.  You should use `eval "${cognito-agent}"` to avoid having the credentials appear in bash history and instead have them persisted into the environment ready to be used with subsequent commands, (either with `awscurl` or the normal `aws` cli).
---
+++ index.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+const { cli } = require('cli-ux');
+const AWS = require('aws-sdk');
+const {
+  CognitoUser,
+  CognitoUserPool,
+  AuthenticationDetails
+} = require('amazon-cognito-identity-js');
+
+(async () => {
+  const COGNITO_AGENT_USERNAME = process.env.COGNITO_AGENT_USERNAME || await cli.prompt('Username');
+  const COGNITO_AGENT_PASSWORD = process.env.COGNITO_AGENT_PASSWORD || await cli.prompt('Password', { type: 'hide' });
+  const COGNITO_AGENT_USERPOOL_ID = process.env.COGNITO_AGENT_USERPOOL_ID || await cli.prompt('UserPoolId');
+  const COGNITO_AGENT_CLIENT_ID = process.env.COGNITO_AGENT_CLIENT_ID || await cli.prompt('ClientId');
+  const COGNITO_AGENT_IDENTITYPOOL_ID = process.env.COGNITO_AGENT_IDENTITYPOOL_ID || await cli.prompt('IdentityPoolId');
+  const COGNITO_AGENT_REGION = COGNITO_AGENT_IDENTITYPOOL_ID.split(':')[0];
+
+  const authenticationDetails = new AuthenticationDetails(
+    {
+      Username: COGNITO_AGENT_USERNAME,
+      Password: COGNITO_AGENT_PASSWORD
+    }
+  );
+  const cognitoUser = new CognitoUser({
+    Username: COGNITO_AGENT_USERNAME,
+    Pool: new CognitoUserPool({
+      UserPoolId: COGNITO_AGENT_USERPOOL_ID,
+      ClientId: COGNITO_AGENT_CLIENT_ID
+    })
+  });
+
+  cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: function (result) {
+      AWS.config.region = COGNITO_AGENT_REGION;
+
+      AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+        IdentityPoolId: COGNITO_AGENT_IDENTITYPOOL_ID,
+        Logins: {
+          [`cognito-idp.${COGNITO_AGENT_REGION}.amazonaws.com/${COGNITO_AGENT_USERPOOL_ID}`]: result.getIdToken().getJwtToken()
+        }
+      });
+
+      AWS.config.credentials.refresh(async (error) => {
+        if (error) {
+          console.error(error);
+        } else {
+          console.log(`AWS_DEFAULT_REGION=${AWS.config.region}; export AWS_DEFAULT_REGION;`);
+          console.log(`AWS_ACCESS_KEY_ID=${AWS.config.credentials.accessKeyId}; export AWS_ACCESS_KEY_ID;`);
+          console.log(`AWS_SECRET_ACCESS_KEY=${AWS.config.credentials.secretAccessKey}; export AWS_SECRET_ACCESS_KEY;`);
+          console.log(`AWS_SESSION_TOKEN=${AWS.config.credentials.sessionToken}; export AWS_SESSION_TOKEN;`);
+        }
+      });
+    },
+    onFailure: function (err) {
+      console.error(err.message || JSON.stringify(err));
+    }
+  });
+})();
